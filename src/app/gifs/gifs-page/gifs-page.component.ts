c34f918b84fcb37abeb02c329bb1b86fa80fc829
theCrims0n <mi.salomon89@gmail.com>
2021-12-15 17:19:31 -0700

    gifsss

--- src/app/gifs/gifs-page/gifs-page.component.ts
+++ src/app/gifs/gifs-page/gifs-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gifs-page',
   templateUrl: './gifs-page.component.html',
-  styleUrls: ['./gifs-page.component.css']
+  styleUrls: ['./gifs-page.component.css'] 
 })
 export class GifsPageComponent implements OnInit {
 
